ae99f152e110366aadca187ffcf4806a6e2b22a4
ricky <rjbenavides@cpp.edu>
2020-11-13 11:28:38 -0800

    Added ability to delete your own post.

--- graphql/resolvers/postsResolvers.js
+++ graphql/resolvers/postsResolvers.js
@@ -1,3 +1,5 @@
+const { AuthenticationError } = require("apollo-server");
+
 const Post = require("../../models/postModel.js");
 const checkAuth = require("../../utils/checkAuth.js");
 
@@ -40,6 +42,20 @@ module.exports = {
       const post = await newPost.save();
 
       return post;
+    },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return "Post deleted succcessfully";
+        } else {
+          throw new AuthenticationError("Action not allowed");
+        }
+      } catch (error) {
+        throw new Error(error);
+      }
     },
   },
 };
