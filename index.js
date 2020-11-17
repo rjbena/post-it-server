bd11fb6f4fd345bb33a383b3005053b25c56d6c1
ricky <rjbenavides@cpp.edu>
2020-11-17 15:09:41 -0800

    Changed env variable

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const server = new ApolloServer({
   context: ({ req }) => ({ req, pubsub }),
 });
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI, {
